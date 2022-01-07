1132211a6c1b56f83a823e7058fe1fc22e0bc6e9
George Fox <georgefox1996@gmail.com>
2022-01-07 20:49:48 +0000

    initial commit

---
+++ football-stats-api/Controllers/PlayersController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using football_stats_api.Data;
+using football_stats_api.Models;
+
+namespace football_stats_api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayersController : ControllerBase
+    {
+        private readonly football_stats_apiContext _context;
+
+        public PlayersController(football_stats_apiContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Players
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Player>>> GetPlayer()
+        {
+            return await _context.Player.ToListAsync();
+        }
+
+        // GET: api/Players/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Player>> GetPlayer(int id)
+        {
+            var player = await _context.Player.FindAsync(id);
+
+            if (player == null)
+            {
+                return NotFound();
+            }
+
+            return player;
+        }
+
+        // PUT: api/Players/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPlayer(int id, Player player)
+        {
+            if (id != player.id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(player).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PlayerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Players
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Player>> PostPlayer(Player player)
+        {
+            _context.Player.Add(player);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPlayer", new { id = player.id }, player);
+        }
+
+        // DELETE: api/Players/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePlayer(int id)
+        {
+            var player = await _context.Player.FindAsync(id);
+            if (player == null)
+            {
+                return NotFound();
+            }
+
+            _context.Player.Remove(player);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PlayerExists(int id)
+        {
+            return _context.Player.Any(e => e.id == id);
+        }
+    }
+}
---
+++ football-stats-api/Models/Player.cs
@@ -0,0 +1,83 @@
+﻿namespace football_stats_api.Models
+{
+    public class Player
+    {
+        //General
+        public int id { get; set; }
+        public string playerName { get; set; }
+        public string playerNation { get; set; }
+        public string playerPosition { get; set; }
+        public string playerAge { get; set; }
+        public string playerTeam { get; set; }
+
+        //Playing time
+        public int matchesPlayed { get; set; }
+        public int matchesStarted { get; set; }
+        public int minutesPlayed { get; set; }
+
+        //Performance
+        public int goals { get; set; }
+        public int assists { get; set; }
+
+        //Expected performance
+        public double expectedGoals { get; set; }
+        public double expectedAssists { get; set; }
+
+        //Shooting
+        public int shots { get; set; }
+        public int shotsOnTarget { get; set; }
+        public double shotDistance { get; set; }
+        public int freeKickShots { get; set; }
+        public int penaltyScored { get; set; }
+        public int penaltyShots { get; set; }
+
+        //Passing
+        public int totalPassesAttempted { get; set; }
+        public int totalPassesCompleted { get; set; }
+        public int shortPassesAttempted { get; set; }
+        public int shortPassesCompleted { get; set; }
+        public int mediumPassesAttempted { get; set; }
+        public int mediumPassesCompleted { get; set; }
+        public int LongPassesAttempted { get; set; }
+        public int LongPassesCompleted { get; set; }
+        public int progressivePassingDistance { get; set; }
+        public int crosses { get; set; }
+
+
+        //Defensive
+        public int tacklesAttempted { get; set; }
+        public int tacklesWon { get; set; }
+        public int tacklesDefensiveThird { get; set; }
+        public int tacklesMiddleThird { get; set; }
+        public int tacklesAttackingThird { get; set; }
+        public int interceptions { get; set; }
+        public int pressuresAttempted { get; set; }
+        public int pressuresWon { get; set; }
+        public int pressuresDefensiveThird { get; set; }
+        public int pressuresMiddleThird { get; set; }
+        public int pressuresAttackingThird { get; set; }
+        public int blocks { get; set; }
+        public int fouls { get; set; }
+
+        //Dribbling
+        public int dribblesAttempted { get; set; }
+        public int dribblesCompleted { get; set; }
+        public int dribblesProgressiveDistance { get; set; }
+        public int progressiveDribbles { get; set; }
+        public int passesReceived { get; set; }
+        public int passesControlled { get; set; }
+
+        //Misc
+        public int yellowCards { get; set; }
+        public int RedCards { get; set; }
+        public int headersWon { get; set; }
+        public int headersLost { get; set; }
+        
+        //General Transfer
+        public int height { get; set; }
+        public string preferredFoot { get; set; }
+        public string contractEndDate { get; set; }
+        public int marketValue { get; set; }
+        public int wage { get; set; }
+    }
+}
